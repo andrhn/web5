6a1ce3d7f5eccc533323309ca4b7927a87388fa0
Katherine Andrhn <katherine.andrhn@gmail.com>
2019-05-03 13:49:08 +0300

    editor only

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+<div id="app">
+  <div class = "contaier">
+  	<div class="row mt-4">
+ 		 <div class = "col-md-6">
+  			<h4 class = "light">Markdown</h4>
+  			<textarea class ="info"
+  					v-model = 'md_text'></textarea>
+ 		 </div>
+
+ 		 <div class = "col-md-6">
+  			<h4 class = "light">Preview</h4>
+  			<div class = "info" v-html='previewText'></div>
+ 		 </div>
+	</div>
+
   </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+let marked = require('marked');
 export default {
   name: "app",
-  components: {
-    HelloWorld
-  }
+  data () {
+
+  return {
+  		md_text : '#### hello world ',
+  	};
+  } ,
+    computed: {
+	previewText() {
+		  marked.setOptions({
+		  renderer: new marked.Renderer(),
+		  gfm: true,
+		  tables: true,
+		  breaks: true,
+		  pedantic: false,
+		  sanitize: true,
+		  smartLists: true,
+		  smartypants: false
+		});
+		return marked(this.md_text)
+	}
+  },
 };
 </script>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+textarea {
+	resize: none;
+	border: 2px dashed yellow;
+	outline: none;
 }
+
+.info {
+	height: 400px;
+	width: 100%;
+	background-color: rgba(255, 255, 255, 0.9);
+}
+
 </style>
